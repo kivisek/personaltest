e40e75c6a85456c5a67e1f1a7456ef1aac2f1e42
kivisek <vikijan1@seznam.cz>
2025-01-20 20:08:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+﻿document.onkeydown = (e) => {
+    if (e.key == 'F1') {
+        e.preventDefault();
+    }
+    if (e.key == 'F2') {
+        e.preventDefault();
+    }
+    if (e.key == 'F3') {
+        e.preventDefault();
+    }
+    if (e.key == 'F4') {
+        e.preventDefault();
+    }
+    if (e.key == 'F6') {
+        e.preventDefault();
+    }
+    if (e.key == 'F7') {
+        e.preventDefault();
+    }
+    if (e.key == 'F8') {
+        e.preventDefault();
+    }
+    if (e.key == 'F9') {
+        e.preventDefault();
+    }
+    if (e.key == 'F10') {
+        e.preventDefault();
+    }
+    if (e.key == 'F12') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
